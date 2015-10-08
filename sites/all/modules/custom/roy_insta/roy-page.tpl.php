288e14b8d88afb46e05e5321c99cbf1b8632de04
roysaguid <rasaguid@gmail.com>
2015-10-08 11:56:58 +0800

    fixed search on insta roy

--- sites/all/modules/custom/roy_insta/roy-page.tpl.php
+++ sites/all/modules/custom/roy_insta/roy-page.tpl.php
@@ -1,5 +1,23 @@
 <?php
 
+//$var1 = 'asdasd';
+//if (isset($var1)){
+//	echo $var1;
+//}
+
+?>
+
+</br></br>
+<?php foreach($insta as $item): ?>
+  <div class="item"> 
+  	<?php print $item['caption']; ?> </br>
+  	<img src="<?php print $item['imgpath']; ?>" /> </br> </br>
+  </div>
+<?php endforeach; ?>
+
+<?php
+
+
 //$imgpath = 'http://storage2.static.itmages.com/i/15/1001/h_1443669059_1381872_6e8a236c63.jpeg';
 // 	$form['test_image_markup'] = array(
 //	'#markup' => '<img src="'.$imgpath.'"/>' 
@@ -15,16 +33,16 @@
 	
 	//echo count($insta['imgpath']);
 
+/*
+
 	for ($i = 0; $i < count($insta['imgpath']); $i++) {
     	//echo "The number is: $x <br>";
     	$imgpath = $insta['imgpath'][$i];//$info['images']['standard_resolution']['url'];
 	    $caption = $insta['caption'][$i];//$info['caption']['text'];
-		//echo $feeds['imgpath'];
-	//	echo $feeds['caption'];
 	    echo $caption."</br>";
 	    echo "<img src='$imgpath' />"."</br></br></br>";
 	} 
-
+*/
 	//$feeds = array();
 	//foreach($insta as $feeds) {
 		
